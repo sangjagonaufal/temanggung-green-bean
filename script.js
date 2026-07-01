4461c78a820f3ceb24792101e38eb628de7c8c0d
sangjagonaufal <sangjagonaufal@gmail.com>
2026-07-02 02:15:31 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const tombol = document.querySelector("button");
+
+    tombol.addEventListener("click", function () {
+        alert("Selamat datang di Temanggung Green Bean ☕");
+    });
+});
